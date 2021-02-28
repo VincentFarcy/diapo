8a9de36fde73415d434cacfec591381d62a434b0
VincentFarcy <v.farcy72@gmail.com>
2021-02-28 11:34:46 +0100

    doctrine upgrade

--- src/Repository/AlbumRepository.php
+++ src/Repository/AlbumRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Album;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Album|null find($id, $lockMode = null, $lockVersion = null)
